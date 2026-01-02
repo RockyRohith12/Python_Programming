b3f0ab8136b2f099d5bfa181c17fe77c65ed5a36
Rohith <rohithrohith0507@gmail.com>
2026-01-02 19:14:29 +0530

    Added new python files and removed old ones

--- print.py
+++
@@ -1,4 +0,0 @@
-#This is my first program 
-
-print("My name is Rocky")
-print("Its working")
--- variable.py
+++
@@ -1,9 +0,0 @@
-# Variables are named memory locations to store data 
-
-
-#Strings 
-first_name = "Rohith"
-food = "Pizza"
-print(f"Hello {first_name}. You like {food}")
-
-#Integers  

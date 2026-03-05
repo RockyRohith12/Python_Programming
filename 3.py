c4ce5dc7751c5269702dd360918d5f77bce0db89
Rohith <rohithrohith0507@gmail.com>
2026-03-05 05:48:31 +0530

    Day 4 = First Python Program

---
+++ 3.py
@@ -0,0 +1,7 @@
+# to install any library we need to use "pip install library_name".
+# to import them use "import library_name".
+# "pip list" gibes u entire python libraries installed on vscode.
+# venv = virtual environment.
+
+import pandas 
+import numpy

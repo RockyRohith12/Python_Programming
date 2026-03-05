8484cbeb306cf4c07be8a8a60b00d095e56ee882
Rohith <rohithrohith0507@gmail.com>
2026-03-05 06:01:44 +0530

    Updated 4.py where I learnt about print statements.

--- 4.py
+++ 4.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# my first python program 
+
+print("Hello World")  #Hello World
+print(5)  #5
+print("Rohith", 1226)  #Rohith 1226
+print(17*12)  #204
+
+print("With you, the long way home never felt long enough")
+print("In idle dreams of long ago, I imagined my true love; A perfect match, a soulmate, An angel from above. Now you're here, and now I know. Our love will stay and thrive and grow")

d03382e3967a2e1de6fbebb98cae33943c5136fa
Rohith <rohithrohith0507@gmail.com>
2026-03-06 06:57:01 +0530

    Day 5 updated with comments and \n

---
+++ 5.py
@@ -0,0 +1,15 @@
+# Comments, escape sequence with print statements 
+# Comments of single line use hash
+# Control + forward slash = comment and uncomment 
+# Multiline comments use triple single coat at the start and end of comments 
+# \n is a escape sequence character which shifts the sentence to next line.
+
+print("Hey, I am Rohith \nMy nickname is Rocky")
+
+print("This a print statement.") #prints the statement 
+
+
+'''
+This is a multiline comment
+
+'''

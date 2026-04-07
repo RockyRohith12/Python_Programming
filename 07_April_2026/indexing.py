b9373de56bb5a34bbcbc3722655d8933b8928c78
Rohith <rohithrohith0507@gmail.com>
2026-04-07 10:26:08 +0530

    Updated on 7th April with indexing topic

---
+++ 07_April_2026/indexing.py
@@ -0,0 +1,42 @@
+''' 
+Python stores strings in continuous memory locations
+We can access every single element of the string 
+
+forward indexing starts from 0 and ends at n-1
+backwards indexing starts from -1 
+
+'''
+
+s = "Python"
+
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+print(s[5])
+
+
+#space is also considered as character in python 
+
+s = 'Rocky Rohith'
+print(s[5])      #space is printed 
+
+print(len(s))
+print(s[len(s)-1])
+
+
+#backward indexing
+
+s = "Python"
+
+print(s[-1])
+print(s[-2])
+print(s[-3])
+print(s[-4])
+print(s[-5])
+print(s[-6])
+
+
+
+# https://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqa0F3NHZDWjFqVDl2N2k1VndvRm1FU0pDRU5Yd3xBQ3Jtc0ttb1Y5Ny1ZNE1SNWZ6VXFNMm9aa09rYm9xRVFXVnc0YUllQWVYcEZySlZLWEtqcF82aTFUVnRtT2c4Q1E5QWdBWUNoamYzTDZRVE95RkZBMnVPM0x2eVFjNWEydFlYSHYzNDNGWkJHMzNBTS1qdkRPQQ&q=https%3A%2F%2Fdrive.google.com%2Fdrive%2Ffolders%2F1hnSWPQC-AaBCttXQ7xQcDkkxLm_XbQJJ%3Fusp%3Dsharing
---
+++ 07_April_2026/string.py
@@ -0,0 +1,20 @@
+# sequence of characters 
+# python do not have char data set 
+
+a="Guvi"
+a='Gumma'
+print(type(a))
+print(a)
+
+#single line string 
+
+print("Hey I'm a single line string")
+
+# \n -> new line 
+# \b -> backspace 
+# \t -> tab space 
+
+print("Hi I am a Multi line sting. \nI'm its next line.")
+
+print(''' Hi i am a multi line
+      string....''')
